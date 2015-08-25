cc4ecbd72a1ff5aefc502e30103c6bd3292851c5
Guillaume Leroi <guillaume-externe.leroi@erdf.fr>
2015-08-25 17:18:23 +0200

    selnet app

---
+++ Selfnet.App/ItemsStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selfnet.App
+{
+    class ItemsStore
+    {
+        public ICollection<Item> All()
+        {
+            return new List<Item>();
+        }
+    }
+}
---
+++ Selfnet.App/SelfnetApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selfnet.App
+{
+    class SelfnetApp
+    {
+        public IEnumerable<Item> GetItems()
+        {
+            return new List<Item>();
+        }
+    }
+}
