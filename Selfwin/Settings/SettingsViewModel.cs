ef86574ec4c3e8f907c561d8346af905413f2ba5
Guillaume Leroi <leroi.g@gmail.com>
2016-02-13 15:52:26 +0100

    use listview for enabling virtualization

--- Selfwin/Settings/SettingsViewModel.cs
+++ Selfwin/Settings/SettingsViewModel.cs
@@ -4,5 +4,52 @@ namespace Selfwin.Settings
 {
     public class SettingsViewModel : Screen
     {
+        private string _url;
+        public string Url
+        {
+            get { return _url; }
+            set
+            {
+                if (value == _url) return;
+                _url = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        private int _port;
+        public int Port
+        {
+            get { return _port; }
+            set
+            {
+                if (value == _port) return;
+                _port = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                if (value == _username) return;
+                _username = value;
+                NotifyOfPropertyChange(() => Username);
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (value == _password) return;
+                _password = value;
+                NotifyOfPropertyChange();
+            }
+        }
     }
 }
